ad9d25728b05b79c0c3042b9ecf5d04627f24b28
Psyh2409 <psyh2409@gmail.com>
2021-11-09 14:55:32 +0200

    simplify solution

--- script.js
+++ script.js
@@ -64,54 +64,12 @@ function _NaNNullEmpty(x) {
     return isNaN(x) || x == null || x == '';
 }
 function solution() {
-    let a, b, operation, answer, result;
-    a = validator(
-        "Put the first number. Or put \'stop\'",
-        _NaNNullEmpty(a));
-    if (a === "stop") {
-        return;
-    }
-    b = validator(
-        "Put the second number, please. Or put \'stop\'",
-        _NaNNullEmpty(b));
-    if (b === "stop") {
-        return;
-    }
-    oper: while (true) {
-        operation = prompt(
-            'Which Math operation do you want to do from \'+\', \'-\', \'*\', \'/\'. Or put \'stop\'');
-        switch (operation) {
-            case "stop":
-                return;
-            case "+":
-                result = parseInt(a) + parseInt(b);
-                break oper;
-            case "-":
-                result = parseInt(a) - parseInt(b);
-                break oper;
-            case "*":
-                result = parseInt(a) * parseInt(b);
-                break oper;
-            case "/":
-                if (parseInt(b) === 0) {
-                    alert("You can\'t devide on zero!");
-                    continue oper;
-                } else {
-                    result = parseInt(a) / parseInt(b);
-                    break oper;
-                }
-            default:
-                console.log(
-                    `${operation} is not Math operation from \'+\', \'-\', \'*\', \'/\'. Be more attention. Or put \'stop\'`);
-                alert(`${operation} is not Math operation from \'+\', \'-\', \'*\', \'/\'. Be more attention. Or put \'stop\'`);
-                continue oper;
-        }
-    }
+    let answer=8*9, result;
     do {
-        answer = validator(
-            `So it\'s solution time! ${a}${operation}${b} = ... Or put \'stop\'`,
-            _NaNNullEmpty(answer));
-        if (answer === "stop") { return; }
+        result = validator(
+            `So it\'s solution time! 8*9 = ... Or put \'stop\'`,
+            _NaNNullEmpty(result));
+        if (result === "stop") { return; }
     } while (answer != result);
     alert(answer);
 }
@@ -153,9 +111,9 @@ function fibonachi(number) {
     alert(strng);
 }
 // main
-adder();
-minusOplus();
+// adder();
+// minusOplus();
 solution();
-authorization(1323);
-fibonachi(10);
+// authorization(1323);
+// fibonachi(10);
 
